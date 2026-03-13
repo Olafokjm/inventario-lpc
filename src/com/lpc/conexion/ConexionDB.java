6b2f31dd59055e71a32b56966a2e2aa5789ef354
Olafokjm <juliokjm@gmail.com>
2026-03-12 20:19:35 -0500

    remove p

--- src/com/lpc/conexion/ConexionDB.java
+++ src/com/lpc/conexion/ConexionDB.java
@@ -7,7 +7,7 @@ public class ConexionDB {
 
     private static final String URL = "jdbc:mysql://localhost:3306/inventario_lpc";
     private static final String USER = "root";
-    private static final String PASSWORD = "GunnerY";
+    private static final String PASSWORD = "";
 
     public static Connection conectar() {
         Connection conexion = null;
